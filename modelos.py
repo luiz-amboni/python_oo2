e5f328e328f9729fd8e48c4dcc96f3243936224b
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-05-27 00:38:11 -0300

    Adicionado funções e métodos nas classes e subclasses

--- modelos.py
+++ modelos.py
@@ -19,18 +19,29 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Likes: {self._likes}'
+
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano}- Duração: {self.duracao} min - Likes: {self._likes}'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano}- Temporadas: {self.temporadas} - Likes: {self._likes}'
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+
 vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
@@ -38,5 +49,7 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
