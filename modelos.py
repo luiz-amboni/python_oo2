08ad98a81fcccb91864406dfc7d4d3df5575f2d9
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-05-28 00:16:37 -0300

    Adicionado classes playlist

--- modelos.py
+++ modelos.py
@@ -39,17 +39,47 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self._nome} - Ano: {self.ano}- Temporadas: {self.temporadas} - Likes: {self._likes}'
 
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
 vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+
+demolidor.dar_likes()
+demolidor.dar_likes()
 
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)} filmes/séries')
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f'Ta ou não ta? {demolidor in playlist_fim_de_semana}')
